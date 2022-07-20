f69067fba9c6ecffe505e29bd75d77ed1b2cb43a
leonardo <leo.carvalholkp@gmail.com>
2022-07-20 19:27:00 -0300

    test: should be able to list all users

---
+++ src/handlers/user-handlers.spec.ts
@@ -0,0 +1,31 @@
+import { User } from "@prisma/client"
+import { prisma } from "../config/prisma-client"
+import request from "supertest"
+import app from "../app"
+
+describe('Given the users resources', () => {
+  let users: User[]
+  describe('GET /users', () => {
+    beforeAll(async () => {
+      await prisma.user.createMany({
+        data: [
+          {
+            name: 'John Doe',
+            email: 'contact@johndoe.com',
+          },
+          {
+            name: 'Jane Doe',
+            email: 'contact@janedoe'
+          }
+        ]
+      })
+      users = await prisma.user.findMany()
+    })
+    it('should be able to list all users', async () =>{
+      const response = await request(app).get('/users')
+
+      expect(response.status).toBe(200)
+      expect(response.body).toMatchObject(users)
+    })
+  })
+})
