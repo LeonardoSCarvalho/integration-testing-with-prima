b47e7ec6cb0f52591233d9d5a33b7681f648369f
leonardo <leo.carvalholkp@gmail.com>
2022-07-20 18:56:10 -0300

    feat: configure jest-global-teardown

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -4,10 +4,12 @@ import crypto from 'crypto';
 import { exec } from 'child_process';
 
 export default async () => {
+  console.info('\nMontando suite de testes...');
   config({ parth: '.env.test' });
   const execSync = util.promisify(exec);
   global.__SCHEMA__ = `test_${crypto.randomUUID()}`
   process.env.DATABASE_URL = `${process.env.DATABASE_URL}?schema=${global.__SCHEMA__}`
 
-  await execSync(`${prismaBinary} migrate deploy`)
+  await execSync(`${prismaBinary} migrate deploy`);
+  console.info('Suite pronta. Iniciando testes...\n');
 }
