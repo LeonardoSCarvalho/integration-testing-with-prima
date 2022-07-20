aa53f1736532954a5521cd24e96099dc0538ae5c
leonardo <leo.carvalholkp@gmail.com>
2022-07-20 19:20:01 -0300

    fix: change path

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -2,10 +2,10 @@ import util from 'util';
 import { config } from 'dotenv';
 import crypto from 'crypto';
 import { exec } from 'child_process';
-
+const prismaBinary = './node_modules/.bin/prisma';
 export default async () => {
   console.info('\nMontando suite de testes...');
-  config({ parth: '.env.test' });
+  config({ path: '.env.test' });
   const execSync = util.promisify(exec);
   global.__SCHEMA__ = `test_${crypto.randomUUID()}`
   process.env.DATABASE_URL = `${process.env.DATABASE_URL}?schema=${global.__SCHEMA__}`
