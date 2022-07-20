580baa70daae4c31803b3cd626308ca2b71771a9
leonardo <leo.carvalholkp@gmail.com>
2022-07-20 20:15:46 -0300

    feat: error handling user

--- src/handlers/user-handlers.ts
+++ src/handlers/user-handlers.ts
@@ -1,21 +1,38 @@
 
-import { Router } from "express";
-import { prisma } from "../config/prisma-client";
+import { Prisma } from "@prisma/client"
+import { Router } from "express"
+import { prisma } from "../config/prisma-client"
 
-
-export const userHandlers = Router();
+const userHandlers = Router()
 
 userHandlers.get('/users', async(request, response) => {
   const users = await prisma.user.findMany()
+
   response.json(users)
 })
+
 userHandlers.post('/users', async(request, response) => {
-  const { name, email } = request.body
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email
+  const { email, name } = request.body
+
+  try {
+    const user = await prisma.user.create({
+      data:{
+        name,
+        email
+      }
+    })
+
+    response.status(201).json(user)
+  } catch (error) {
+    //https://www.prisma.io/docs/reference/api-reference/error-reference#p2002
+    if(error instanceof Prisma.PrismaClientKnownRequestError) {
+      if(error.code === 'P2002') {
+        return response.status(409).json({ message: `The email ${email} is already registered`})
+      }
     }
-  })
-  response.status(201).json(user)
+
+    response.status(500).json({ message: 'Internal server error.'})
+  }
 })
+
+export { userHandlers }
