40f53cddbbf13ae3a57abebe64dceb00be3d2514
leonardo <leo.carvalholkp@gmail.com>
2022-07-20 19:25:37 -0300

    refactor: create app

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,3 @@
-import express  from 'express'
-import { taskHandlers } from './handlers/task-handlers'
-import { userHandlers } from './handlers/user-handlers'
-
-const app = express()
-app.use(express.json())
-app.use(userHandlers)
-app.use(taskHandlers)
+import app from './app'
 
 app.listen(3000, () => console.log('Serve running on port 3000'))
