138bc5d1c04e3e568974cc6a44950712bd57fd00
leonardo <leo.carvalholkp@gmail.com>
2022-07-19 22:13:07 -0300

    feat: create handlers task

---
+++ src/handlers/task-handlers.ts
@@ -0,0 +1,22 @@
+
+import { Router } from "express";
+import { prisma } from "../config/prisma-client";
+
+
+export const taskHandlers = Router();
+
+taskHandlers.get('/tasks', async(request, response) => {
+  const tasks = await prisma.task.findMany()
+  response.json(tasks)
+})
+taskHandlers.post('/tasks', async(request, response) => {
+  const { userId, title, description } = request.body
+  const task = await prisma.task.create({
+    data: {
+      title,
+      userId,
+      description
+    }
+  })
+  response.status(201).json(task)
+})
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express  from 'express'
+import { taskHandlers } from './handlers/task-handlers'
 import { userHandlers } from './handlers/user-handlers'
 
 const app = express()
 app.use(express.json())
 app.use(userHandlers)
+app.use(taskHandlers)
 
 app.listen(3000, () => console.log('Serve running on port 3000'))
